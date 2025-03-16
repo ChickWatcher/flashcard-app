43a40cf8a7916e9f1c3fe0a206bf356ebc272454
Chick Watcher <whyamisonotawesome@gmail.com>
2025-03-15 22:28:51 -0600

    changes

--- backend/run.py
+++ backend/run.py
@@ -1,11 +1,11 @@
-from app import app
+import os
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder="frontend/dist", static_url_path="/")
+app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), "../frontend/dist"), static_url_path="/")
 
 @app.route("/")
 def serve_frontend():
     return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
